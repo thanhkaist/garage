ff48bc36113c13593e5562e96cbf8b03da1a3b95
irisliucy <irisliu.0616@gmail.com>
2020-06-15 21:47:55 -0700

    Add torch categorical policy and modules

--- src/garage/torch/modules/__init__.py
+++ src/garage/torch/modules/__init__.py
@@ -1,5 +1,7 @@
 """Pytorch modules."""
 
+from garage.torch.modules.categorical_cnn_module import CategoricalCNNModule
+from garage.torch.modules.cnn_module import CNNBaseModule
 from garage.torch.modules.gaussian_mlp_module import \
     GaussianMLPIndependentStdModule, GaussianMLPModule, \
     GaussianMLPTwoHeadedModule
@@ -7,6 +9,8 @@ from garage.torch.modules.mlp_module import MLPModule
 from garage.torch.modules.multi_headed_mlp_module import MultiHeadedMLPModule
 
 __all__ = [
+    'CategoricalCNNModule',
+    'CNNBaseModule',
     'MLPModule',
     'MultiHeadedMLPModule',
     'GaussianMLPModule',
